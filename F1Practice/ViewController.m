f5a0cf79b9ae9b7917637a157b0d89966f867cc3
lee-leonardo <ohhumbleleo@gmail.com>
2014-05-06 21:10:20 -0700

    First Commit

--- F1Practice/ViewController.m
+++ F1Practice/ViewController.m
@@ -25,5 +25,23 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)Button:(id)sender {
+	NSLog(@"Hello World!");
+	
+	UIColor *greenColor = [UIColor greenColor];
+	self.view.backgroundColor = greenColor;
+	
+	//self.view.backgroundCOlor [UIColor greenColor];
+	
+}
+- (IBAction)switch:(id)sender {
+	NSLog(@"I just got switched!");
+}
+- (IBAction)slider:(id)sender {
+	
+	if ( self.slider.value > 0.5) {
+		NSLog(@"The slider value is: %f.", self.slider.value);
+	}
+}
 
 @end
