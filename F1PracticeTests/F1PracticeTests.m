ad72dbdee4317f483dbce78c959fde83126f19d3
lee-leonardo <ohhumbleleo@gmail.com>
2014-05-06 19:55:10 -0700

    Initial Commit

---
+++ F1PracticeTests/F1PracticeTests.m
@@ -0,0 +1,34 @@
+//
+//  F1PracticeTests.m
+//  F1PracticeTests
+//
+//  Created by Leonardo Lee on 5/6/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface F1PracticeTests : XCTestCase
+
+@end
+
+@implementation F1PracticeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
